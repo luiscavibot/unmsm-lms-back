7a9d36d1e3effc72f5c54b7480914cc755730fac
Monica Escriba <monicaescriba.t6@gmail.com>
2025-05-03 18:30:23 -0500

    LMS-29 feat: delete ProgramCourse module

--- src/modules/program-courses/interfaces/program-course.repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { ProgramCourse } from '../entities/program-course.entity';
-
-export interface IProgramCourseRepository {
-  create(programCourse: ProgramCourse): Promise<ProgramCourse>;
-  findAll(): Promise<ProgramCourse[]>;
-  findOne(id: string): Promise<ProgramCourse | null>;
-  update(id: string, programCourse: Partial<ProgramCourse>): Promise<ProgramCourse | null>;
-  delete(id: string): Promise<void>;
-}
