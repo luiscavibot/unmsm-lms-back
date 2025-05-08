8375bb51fae10612a694c26be9645b85c9119df1
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2025-05-08 14:39:57 -0500

    refactor: Remove unused UserPayload import from current user decorator

--- src/common/auth/decorators/current-user.decorator.ts
+++ src/common/auth/decorators/current-user.decorator.ts
@@ -1,7 +1,4 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserPayload } from '../interfaces';
-
-// UserPayload
 
 export const CurrentUserToken = createParamDecorator((_data: unknown | undefined, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
