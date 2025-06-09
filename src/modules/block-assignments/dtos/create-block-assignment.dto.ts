c3b565b9862d094dedc11c45919c82edae70aa50
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2025-06-09 11:08:43 -0500

    feat: Cambiar la validación de userId a cadena en CreateBlockAssignmentDto

--- src/modules/block-assignments/dtos/create-block-assignment.dto.ts
+++ src/modules/block-assignments/dtos/create-block-assignment.dto.ts
@@ -1,10 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsNotEmpty, IsUUID } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { BlockRolType } from '../enums/block-rol-type.enum';
 
 export class CreateBlockAssignmentDto {
   @IsNotEmpty()
-  @IsUUID()
+  @IsString()
   @ApiProperty({
     description: 'ID del usuario',
     example: '123e4567-e89b-12d3-a456-426614174000',
